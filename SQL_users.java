c85b5a5f054680e0a364d58afcccd95345f559ba
Jan Bertram <jzqk@mailbox.org>
2021-01-16 17:09:09 +0100

    Methode createUser erzeugt Liste, die in Datei geschrieben werden kann.

--- SQL_users.java
+++ SQL_users.java
@@ -24,13 +24,22 @@ public class SQL_users {
         Files.write(file, toWrite, StandardCharsets.UTF_8);
     }
 
+    public static List<String> createUser(List<String> userList) {
+        ArrayList<String> output = new ArrayList<String>();
+        for (String line : userList) {
+            String[] userdata = line.split(";");
+            output.add(String.format("CREATE USER %s IDENTIFIED BY %s;", userdata[0], userdata[1]));
+        }
+        return output;
+    }
+
     public static void main(String[] args) throws IOException {
         List<String> lines = new ArrayList<String>();
         lines = readUserFile("userexample.csv");
         for (String line : lines) {
             System.out.println(line);
         }
-        writeListToFile(lines, "out.txt");
+        writeListToFile(createUser(lines), "out.txt");
     }
 
 }
